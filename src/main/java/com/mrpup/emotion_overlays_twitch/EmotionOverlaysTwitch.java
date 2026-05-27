5e5c42ebbf63750803436803f2125e1a40d14ed1
MrPup-minecraftkus <nazarvalcuk1@gmail.com>
2026-05-27 22:37:50 +0300

    neoforge-1.21.1

--- src/main/java/com/mrpup/emotion_overlays_twitch/EmotionOverlaysTwitch.java
+++ src/main/java/com/mrpup/emotion_overlays_twitch/EmotionOverlaysTwitch.java
@@ -4,10 +4,12 @@ import com.mrpup.emotion_overlays.client.EmojiTextureManager;
 import com.mrpup.emotion_overlays.common.EmojiRegistry;
 import com.mrpup.emotion_overlays_twitch.tvAddon.SevenTVLoader;
 import com.mrpup.emotion_overlays_twitch.tvAddon.SevenTVTextureLoader;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
 
 @Mod(EmotionOverlaysTwitch.MOD_ID)
 public class EmotionOverlaysTwitch {
@@ -15,11 +17,12 @@ public class EmotionOverlaysTwitch {
     public static final String MOD_ID = "emotion_overlays_twitch";
 
     public EmotionOverlaysTwitch(IEventBus modEventBus, ModContainer modContainer) {
-        EmojiTextureManager.setExternalProvider(new SevenTVTextureLoader());
-        EmojiRegistry.setExternalSearchHook(query ->
-                SevenTVLoader.searchEmotes());
-        SevenTVLoader.loadGlobalEmotes();
-        SevenTVLoader.searchEmotes();
+       if (FMLEnvironment.dist == Dist.CLIENT) {
+           EmojiTextureManager.setExternalProvider(new SevenTVTextureLoader());
+           EmojiRegistry.setExternalSearchHook(query -> SevenTVLoader.searchEmotes());
+           SevenTVLoader.loadGlobalEmotes();
+           SevenTVLoader.searchEmotes();
+       }
 
         modEventBus.addListener(this::commonSetup);
     }
